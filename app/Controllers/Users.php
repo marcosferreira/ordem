7764a27110de8270386e0756551c719b504dbf30
Marcos Ferreira <marcosferreira.developer@gmail.com>
2023-12-18 13:07:20 -0300

    implement getUsers method in Users Controller, to ajax request

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -23,4 +23,23 @@ class Users extends BaseController
 
         return view('Users/index', $data);
     }
+
+    public function getUsers() {
+        if (!$this->request->isAJAX()) {
+            return redirect()->back();
+        }
+
+        $attr = [
+            'id',
+            'name',
+            'email',
+            'active',
+            'thumbnail',
+        ];
+
+        $userList = $this->userModel->select($attr)->findAll();
+
+        echo "<pre>";
+        print_r($userList);
+    }
 }
