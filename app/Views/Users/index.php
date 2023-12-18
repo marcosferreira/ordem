38e2d28fed68b8f038940f197f302a977702eb5e
Marcos Ferreira <marcosferreira.developer@gmail.com>
2023-12-18 15:43:14 -0300

    add DataTables translations

--- app/Views/Users/index.php
+++ app/Views/Users/index.php
@@ -30,7 +30,38 @@
 
 <?php echo $this->section('scripts'); ?>
 <script>
+    const DATA_TABLES_PT_BR = {
+        "sEmptyTable": "Nenhum registro encontrado",
+        "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registro",
+        "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+        "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sInfoThousands": ".",
+        "sLengthMenu": "_MENU_ Resultado por página",
+        "sLoadingRecords": "Carregando...",
+        "sProcessing": "Processando...",
+        "sZeroRecords": "Nenhum registro encontrado",
+        "sSearch": "Pesquisa",
+        "oPaginate": {
+            "sNext": "Próximo",
+            "sPrevious": "Anterior",
+            "sFirst": "Primeiro",
+            "sLast": "Último",
+        },
+        "oAria": {
+            "sSortAscending": "Ordenar colunas de forma ascendente",
+            "sSortDescending": "Ordenar colunas de forma descendente",
+        },
+        "select": {
+            "rows": {
+                "_": "Selecionado %d linhas",
+                "0": "Nenhuma linha selecionada",
+                "1": "Selecionado 1 linha",
+            }
+        },
+    };
     new DataTable('#ajaxTable', {
+        oLanguage: DATA_TABLES_PT_BR,
         ajax: '<?php echo base_url('users/getUsers'); ?>',
         columns: [{
                 data: 'imagem'
