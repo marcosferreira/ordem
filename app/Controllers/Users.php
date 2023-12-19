f9e43daf63fa46e8f56390d37ed9d274728b77ef
Marcos Ferreira <marcosferreira.developer@gmail.com>
2023-12-19 08:25:39 -0300

    create methods show() and showOr404(), in Users Controller

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\UserModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Users extends BaseController
 {
@@ -24,7 +25,8 @@ class Users extends BaseController
         return view('Users/index', $data);
     }
 
-    public function getUsers() {
+    public function getUsers()
+    {
         if (!$this->request->isAJAX()) return redirect()->back();
 
         $attr = [
@@ -41,9 +43,10 @@ class Users extends BaseController
         $data = [];
 
         foreach ($usersList as $user) {
+            $userName = esc($user->name);
             $data[] = [
                 'imagem' => $user->thumbnail,
-                'nome' => esc($user->name),
+                'nome' => anchor("users/show/$user->id", esc($user->name), 'title="Exibir usuário: ' . esc($user->name) . '"'),
                 'email' => esc($user->email),
                 'ativo' => ($user->active == true ? '<span class="text-success"><i class="fa fa-unlock"></i> Ativo</span>' : '<span class="text-warning"><i class="fa fa-lock"></i> Inativo</span>'),
             ];
@@ -54,5 +57,32 @@ class Users extends BaseController
         ];
 
         return $this->response->setJSON($response);
+    }
+
+    public function show(int $id = null)
+    {
+        $user = $this->showOr404($id);
+
+        $data = [
+            'title' => "Detalhes do usuário ".esc($user->name),
+            'user' => $user,
+        ];
+
+        return view('Users/show', $data);
+    }
+
+    /**
+     * Method get user
+     * 
+     * @param integer $id
+     * @return PageNotFoundException|object
+     */
+    private function showOr404(int $id = null)
+    {
+        if (!$id || !$user = $this->userModel->withDeleted(true)->find($id)) {
+            throw PageNotFoundException::forPageNotFound("Usuário $id não encontrado");
+        }
+        
+        return $user;
     }
 }
