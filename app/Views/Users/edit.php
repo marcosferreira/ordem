dc0e13378df96ed07c8a0669766a67e55b346b78
Marcos Ferreira <marcosferreira.developer@gmail.com>
2023-12-20 20:43:46 -0300

    add helper form in view/user/edit, with jQuery Ajax

--- app/Views/Users/edit.php
+++ app/Views/Users/edit.php
@@ -17,10 +17,10 @@
 
                 </div>
 
-                <?php echo form_open('/users', ['id' => 'form'], ['id' => "$user->id"]); ?>
+                <?php echo form_open('', ['id' => 'form'], ['id' => "$user->id"]); ?>
                 <?php echo $this->include('Users/inc/form'); ?>
                 <div class="form-group">
-                    <input type="submit" value="Salvar" class="btn btn-primary">
+                    <input type="submit" value="Salvar" id="btn-save" class="btn btn-primary">
                     <a href="<?php echo base_url("users/edit/$user->id") ?>" class="btn btn-secondary">Voltar</a>
                 </div>
                 <?php echo form_close(); ?>
@@ -31,4 +31,39 @@
 <?php echo $this->endSection('content'); ?>
 
 <?php echo $this->section('scripts'); ?>
+<script>
+    $(document).ready(function() {
+        $('#form').on('submit', function(e){
+            e.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: '<?php echo base_url('users/update'); ?>',
+                data: new FormData(this),
+                dataType: 'json',
+                contentType: false,
+                cache: false,
+                processData: false,
+                beforeSend:  function() {
+                    $('#response').html('');
+                    $('#btn-save').val('Por favor aguarde');
+                },
+                success: function(response) {
+                    $('#btn-save').val('Salvar');
+                    $('#btn-save').removeAttr('disabled');
+                    if (!response.erro) {
+                        $('[name=csrf_service_order]').val(response.csrf_token);
+                        if (response.info) {
+                            $('#response').html('<div class="alert alert-primary">' + response.info + '</div>')
+                        }
+                    } else {
+                        // Existem erros de validação
+                    }
+                },
+                error: function() {
+                    alert("Não foi possível processar a solicitação, por favor entre em contato com o suporte técnico.");
+                }
+            });
+        })
+    });
+</script>
 <?php echo $this->endSection('scripts'); ?>
