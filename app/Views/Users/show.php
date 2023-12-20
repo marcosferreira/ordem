90232a27e167e2277ab7dd02efde45686f841bb8
Marcos Ferreira <marcosferreira.developer@gmail.com>
2023-12-20 09:56:19 -0300

    add user data

--- app/Views/Users/show.php
+++ app/Views/Users/show.php
@@ -9,14 +9,37 @@
 
 <?php echo $this->section('content'); ?>
 <div class="row">
-    <div class="block">
-        <div class="text-center">
-            <?php if ($user->thumbnail == null) : ?>
-                <img src="<?php echo base_url('assets') ?>/img/blank-profile.png" alt="" width="180">
-            <?php else : ?>
+    <div class="col-lg-4">
+        <div class="block">
+            <div class="text-center">
+                <?php if ($user->thumbnail == null) : ?>
+                    <img src="<?php echo base_url('assets/img/blank-profile.png'); ?>" alt="Usuário sem imagem" class="card-img-top" width="90%">
+                <?php else : ?>
+                    <img src="<?php echo base_url("users/img/$user->thumbnail"); ?>" alt="<?php echo esc('') ?>" class="card-img-top" width="90%">
+                <?php endif; ?>
 
-            <?php endif; ?>
-        </div>
+                <a href="<?php echo base_url("users/updateImage/$user->id"); ?>" class="btn btn-outline-primary btn-sm mt-3">Alterar imagem</a>
+            </div>
+
+            <hr class="border-secondary">
+
+            <h5 class="card-title"><?php echo $user->name; ?></h5>
+            <p class="card-text">Criado <?php echo $user->created_at; ?></p>
+            <p class="card-text">Atualizado <?php echo $user->updated_at; ?></p>
+
+            <div class="btn-group">
+                <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    Ações
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="<?php echo base_url("users/edit/$user->id"); ?>">Editar usuário</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Separated link</a>
+                </div>
+            </div>
+            <a href="<?php echo base_url("users") ?>" class="btn btn-secondary">Voltar</a>
+        </div> <!-- ./block -->
+
     </div>
 </div>
 <?php echo $this->endSection('content'); ?>
